05009ede1bdad419ffd2ac2aba05fb284afe6f5e
Mohsen Jahed <161829820+Kurayim@users.noreply.github.com>
2025-05-31 02:53:47 -0800

    Update rw_96x8_sync.vhd

--- rw_96x8_sync.vhd
+++ rw_96x8_sync.vhd
@@ -1,1 +1,25 @@
-salam
+Library IEEE;
+use IEEE.STD_LOGICC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+
+entity rw_96x8_sync is
+  port(
+        clock   : in std_logic;
+        reset   : in std_logic;
+        address : in std_logic_vector(6 downto 0);
+        data_in : out std_logic_vector(7 downto 0);
+        );
+end rw_96x8_sync;
+
+architecture Behavioral of rw_96x8_sync is
+
+
+  
+  begin
+
+
+
+
+    
+end Behavioral;
