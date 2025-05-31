a070f179a3787b24e799241d82c50d5fbe8d1acd
Mohsen Jahed <161829820+Kurayim@users.noreply.github.com>
2025-05-31 13:19:37 -0700

    Update output_ports_16.vhd

--- output_ports_16.vhd
+++ output_ports_16.vhd
@@ -1,12 +1,42 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/31/2025 12:39:50 PM
+-- Design Name: 
+-- Module Name: output_ports_16 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 Library IEEE;
-use IEEE.STD_LOGICC_1164.ALL;
+use IEEE.std_logic_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- Entity declaration for synchronous 96x8-bit read/write memory
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
 entity output_ports_16 is
   port(
         clock        : in  std_logic;
         reset        : in  std_logic;
+        write        : in  std_logic;
         address      : in  std_logic_vector(7 downto 0);
         data_in      : in  std_logic_vector(7 downto 0);
         port_out_00  : out std_logic_vector(7 downto 0);
@@ -24,37 +54,38 @@ entity output_ports_16 is
         port_out_12  : out std_logic_vector(7 downto 0);
         port_out_13  : out std_logic_vector(7 downto 0);
         port_out_14  : out std_logic_vector(7 downto 0);
-        port_out_15  : out std_logic_vector(7 downto 0);
+        port_out_15  : out std_logic_vector(7 downto 0)
         );
 end output_ports_16;
-  
+
 architecture Behavioral of output_ports_16 is
 
 
-
-  begin
     
+begin
+
     P0 : process(clock, reset)
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E0")then
             port_out_00 <= data_in;
           end if;
-        end if
+        end if;
     end process;
-
-
-     P1 : process(clock, reset)
+    
+    
+    
+P1 : process(clock, reset)
      begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E1")then
             port_out_01 <= data_in;
           end if;
-        end if
+        end if;
     end process;
 
 
@@ -62,7 +93,7 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E2")then
             port_out_02 <= data_in;
           end if;
@@ -73,7 +104,7 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E3")then
             port_out_03 <= data_in;
           end if;
@@ -84,7 +115,7 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E4")then
             port_out_04 <= data_in;
           end if;
@@ -95,7 +126,7 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E5")then
             port_out_05 <= data_in;
           end if;
@@ -106,7 +137,7 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E6")then
             port_out_06 <= data_in;
           end if;
@@ -117,7 +148,7 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E7")then
             port_out_07 <= data_in;
           end if;
@@ -128,7 +159,7 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E8")then
             port_out_08 <= data_in;
           end if;
@@ -139,7 +170,7 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
+        elsif(rising_edge(clock))then
           if(write = '1'  and  address = X"E9")then
             port_out_09 <= data_in;
           end if;
@@ -150,8 +181,8 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
-          if(write = '1'  and  address = X"E0")then
+        elsif(rising_edge(clock))then
+          if(write = '1'  and  address = X"EA")then
             port_out_10 <= data_in;
           end if;
         end if;
@@ -161,8 +192,8 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
-          if(write = '1'  and  address = X"E0")then
+        elsif(rising_edge(clock))then
+          if(write = '1'  and  address = X"EB")then
             port_out_11 <= data_in;
           end if;
         end if;
@@ -172,8 +203,8 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
-          if(write = '1'  and  address = X"E0")then
+        elsif(rising_edge(clock))then
+          if(write = '1'  and  address = X"EC")then
             port_out_12 <= data_in;
           end if;
         end if;
@@ -183,8 +214,8 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
-          if(write = '1'  and  address = X"E0")then
+        elsif(rising_edge(clock))then
+          if(write = '1'  and  address = X"ED")then
             port_out_13 <= data_in;
           end if;
         end if;
@@ -194,8 +225,8 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
-          if(write = '1'  and  address = X"E0")then
+        elsif(rising_edge(clock))then
+          if(write = '1'  and  address = X"EF")then
             port_out_14 <= data_in;
           end if;
         end if;
@@ -205,15 +236,14 @@ architecture Behavioral of output_ports_16 is
     begin
         if(reset = '0')then
           port_out_00 <= X"00";
-        else if(rising_edge(clock))then
-          if(write = '1'  and  address = X"E0")then
+        elsif(rising_edge(clock))then
+          if(write = '1'  and  address = X"F0")then
             port_out_15 <= data_in;
           end if;
         end if;
     end process;
 
- 
-end Behavioral;    -- End of architecture
+end Behavioral;
 
 
 
@@ -226,4 +256,17 @@ end Behavioral;    -- End of architecture
 
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
