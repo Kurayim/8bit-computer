90c6d5d40b72a7baf71c62de6072ef0dbfebf55d
Mohsen Jahed <161829820+Kurayim@users.noreply.github.com>
2025-06-02 10:52:34 -0700

    Update memory.vhd

--- memory.vhd
+++ memory.vhd
@@ -1,3 +1,36 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06/01/2025 12:34:27 PM
+-- Design Name: 
+-- Module Name: memory - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -177,3 +210,4 @@ begin
     end process;
 
 end Behavioral;
+
