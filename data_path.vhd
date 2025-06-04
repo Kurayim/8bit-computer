c87cf80b5cb986810a95566635865aa7f323ba34
Mohsen Jahed <161829820+Kurayim@users.noreply.github.com>
2025-06-04 01:37:26 -0700

    Update data_path.vhd

--- data_path.vhd
+++ data_path.vhd
@@ -1,3 +1,21 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06/02/2025 12:23:25 PM
+-- Design Name: 
+-- Module Name: data_path - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
 -- ============================================================================
 -- Title      : data_path.vhd
 -- Description: Implements the datapath of an 8-bit CPU including registers, buses,
@@ -9,6 +27,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity data_path is
     Port (
@@ -33,6 +60,7 @@ entity data_path is
 end data_path;
 
 architecture Behavioral of data_path is
+ 
     signal Bus1, Bus2 : std_logic_vector(7 downto 0); -- Internal buses
     signal PC, A, B, MAR, IR_reg : std_logic_vector(7 downto 0); -- Registers
     signal ALU_Result : std_logic_vector(7 downto 0); -- ALU result
@@ -47,8 +75,9 @@ architecture Behavioral of data_path is
             NZVC     : out std_logic_vector(3 downto 0)
         );
     end component;
-
+    
 begin
+
     -- Mux for Bus1: selects PC, A, or B to drive Bus1
     process(Bus1_Sel, PC, A, B)
     begin
